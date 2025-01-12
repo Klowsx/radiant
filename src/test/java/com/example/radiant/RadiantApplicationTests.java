b62de61b055111841156474ea32688ad541c79ff
Javier Hernandez <javierh3r@gmail.com>
2025-01-12 13:19:34 -0500

    first commit

---
+++ src/test/java/com/example/radiant/RadiantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.radiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
